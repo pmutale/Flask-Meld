0e58e6ecd8213a0619831c18f570ab0a8e19c586
Michael Abrahamsen <mail@michaelabrahamsen.com>
2021-12-06 20:16:42 -0700

    add failing test for textarea

--- tests/browser/databinding/default/test_default.py
+++ tests/browser/databinding/default/test_default.py
@@ -8,6 +8,9 @@ def test_input_text(browser_client, page):
     page.wait_for_timeout(200)
     assert page.inner_text("#bound-data-input") == fill_text
 
+    # make sure the text in the input does not disappear
+    assert page.input_value("#input") == fill_text
+
 
 def test_text_area(browser_client, page):
     page.goto("http://127.0.0.1:5009/")
@@ -18,3 +21,6 @@ def test_text_area(browser_client, page):
     page.fill("#text-area", fill_text)
     page.wait_for_timeout(200)
     assert page.inner_text("#bound-data-textarea") == fill_text
+
+    # make sure the text in the input does not disappear
+    assert page.input_value("#text-area") == fill_text
