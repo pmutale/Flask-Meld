c9a20e4c537b31c64398846753a4203c356dc22a
Michael Abrahamsen <mail@michaelabrahamsen.com>
2021-12-19 21:36:07 -0700

    manually set socketio for test project

--- tests/meld_test_project/__init__.py
+++ tests/meld_test_project/__init__.py
@@ -1,6 +1,7 @@
 import secrets
 from flask import Flask, render_template
 from flask_meld import Meld
+from flask_socketio import SocketIO
 
 # extensions
 meld = Meld()
@@ -10,16 +11,12 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SECRET_KEY"] = secrets.token_hex(16)
-
-    meld.init_app(app)
+    app.config["DEBUG"] = False
+    socketio = SocketIO(app, async_mode="threading")
+    meld.init_app(app, socketio)
 
     @app.route("/")
     def index():
         return render_template("index.html")
 
     return app
-
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(port=5001)
